958c5e3d2019df893e19efd1fda10f7b03a0a8eb
spur <symmekol@gmail.com>
2022-10-20 16:21:19 +0800

    first commit

--- src/main/java/com/javasm/Test/GitTest.java
+++ src/main/java/com/javasm/Test/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("master: first commit");
+        System.out.println("master: second commit");
+        System.out.println("hot-fix: first commit");
         System.out.println("and so on...");
     }
 }

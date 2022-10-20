2ce543cd15c0ccffeb3523e440f253f290e2890d
spur <symmekol@gmail.com>
2022-10-20 16:19:55 +0800

    second commit

---
+++ src/main/java/com/javasm/Test/GitTest.java
@@ -0,0 +1,9 @@
+package com.javasm.Test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("master: first commit");
+        System.out.println("and so on...");
+    }
+}
---
+++ src/main/java/com/javasm/entity/User.java
@@ -0,0 +1,9 @@
+package com.javasm.entity;
+
+
+public class User {
+    private Integer uid;
+    private String userName;
+    private String password;
+    private Integer pid;
+}
